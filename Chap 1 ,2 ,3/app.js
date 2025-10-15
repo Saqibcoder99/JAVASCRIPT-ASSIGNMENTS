e76033144a2278657ae6102b36d55b954cdf68e7
Saqibcoder99 <javaidsaqib853@gmail.com>
2025-10-15 21:49:57 +0500

    ASSIGNMENT 1

---
+++ Chap 1 ,2 ,3/app.js
@@ -0,0 +1,66 @@
+
+
+                                 
+                                             //CHAPTER 1 
+    //ANSWER 1   
+   alert("Hello world");  
+     //ANSWER 2                                           
+   alert("Error! Please enter a valid password");
+   //ANSWER 3     
+   alert("Welcome to JS Land...\n Happy Coading!");
+    //ANSWER 4      
+    alert("Welcome to JS Land...");  
+    alert("Happy Coading!")
+  //ANSWER 5 
+    console.log("Hello... I can run JS through my web browser console");
+ //ANSWER 6 , 7
+  DONE
+
+                                              /*CHAPTER 2 */  
+
+ //ANSWER 1 
+ let Username;
+  //ANSWER 2       
+ Username="Saqib javaid";
+  //ANSWER 3 
+ let message;
+message="Hello World";
+ //ANSWER 4 
+let age="19 years old";
+let certified="MWAD";
+alert(Username)
+alert(message)
+alert(age)
+alert(certified)
+ //ANSWER 5
+ let a="PIZZA\n PIZZ\n PIZ\n PI\n P";
+ alert(a)
+  //ANSWER 6 
+  let email="javaidsaqib123@gmail.com";
+  alert( "My name is ",email)
+    //ANSWER 7
+   let Book="A smarter way to learn Javascript";
+   alert(Book) 
+   //ANSWER 8 
+
+    //ANSWER 9 
+
+
+
+
+                                        /*CHAPTER 3 */
+        /*ANSWER 1 */
+     let age="19 years old";                                    
+     alert(age)
+        /*ANSWER 2 */
+     let visit="You have visited this site 25 times";
+     alert(visit)  
+         /*ANSWER 3 */
+   let birthYear="My birth year is 2000";
+   document.writeln(birthYear)
+         /*ANSWER 4*/
+   let store="Ayesha awan stored 10-T shirt(s) on Saqib store";
+   document.writeln(store)
+
+
+               
