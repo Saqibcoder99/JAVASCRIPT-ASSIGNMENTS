d87828d58d98a5b67fdbd7fdc3e673d02cd680b0
Saqibcoder99 <javaidsaqib853@gmail.com>
2025-10-23 07:45:58 +0500

    LOGIC BASE ASSIGNMENT

---
+++ Logic game/app.js
@@ -0,0 +1,14 @@
+   let secret_no=8;
+   user_number=prompt("Enter a number between 1 to 10");
+   if(user_number==secret_no){
+    console.log( "Correct answer");
+   }   
+   else if(user_number==9){
+    console.log("Close enough to the correct answer");
+   }
+    else if(user_number==7){
+    console.log("Close enough to the correct answer");
+   }
+   else{
+    console.log("Incorrect Answer")
+   }
