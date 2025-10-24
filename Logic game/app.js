acb479c65f14f91c9094f5689969eb26b92bef87
Saqibcoder99 <javaidsaqib853@gmail.com>
2025-10-24 08:53:39 +0500

    Update app.js

--- Logic game/app.js
+++ Logic game/app.js
@@ -3,12 +3,20 @@
    if(user_number==secret_no){
     console.log( "Correct answer");
    }   
-   else if(user_number==9){
+   else if(user_number==secret_no+1||user_number==secret_no-1){
     console.log("Close enough to the correct answer");
    }
-    else if(user_number==7){
-    console.log("Close enough to the correct answer");
+    else if(user_number==secret_no+2||user_number==secret_no-2){
+    console.log("qareeb ho rahy ho");
+   }
+      else if(user_number==secret_no-3||user_number==secret_no-4){
+    console.log("Thora sa better guess");
+   }
+    else if(user_number==secret_no-5){
+    console.log("thora sa or try karoo");
+   }   else if(user_number==secret_no-7||user_number==secret_no-6){
+    console.log("lower guess");
    }
    else{
-    console.log("Incorrect Answer")
+    console.log("Invalid Number")
    }
