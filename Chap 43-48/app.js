a721683f76839b26bb4f72e84abc2cc53274deba
Saqibcoder99 <javaidsaqib853@gmail.com>
2026-06-15 09:50:30 +0500

    updated

---
+++ Chap 43-48/app.js
@@ -0,0 +1,14 @@
+function changing(e){
+    e.src="https://tse1.mm.bing.net/th/id/OIP.gUNf2AFHSw-G7QCU8-gM1AAAAA?rs=1&pid=ImgDetMain&o=7&rm=3"
+}
+function reverse(e){
+    e.src="https://tse4.mm.bing.net/th/id/OIP.XX-Si26Nj2JaLUVyG46ffgHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
+}
+function increaseButton(e){
+ let number=Number(e.previousElementSibling);
+ number.innerHTML=5;
+ 
+ console.log(number)
+}
+
+ 
