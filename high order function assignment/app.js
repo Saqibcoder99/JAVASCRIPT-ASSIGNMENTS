e002f57a43b8ce5ef4330a65a26c060c336f78d0
Saqibcoder99 <javaidsaqib853@gmail.com>
2026-06-27 12:41:48 +0500

    asignment

---
+++ high order function assignment/app.js
@@ -0,0 +1,110 @@
+
+
+// ANSWER:1
+//  ek esa function jo dusre function ko apne argument main ly higher order function kehlata ha
+
+// ANSWER:2
+
+//    Yes,map()is a higher order function                         
+
+
+// ANSWER:3
+
+//  No, forEach() does not return a value                            
+
+// ANSWER:4
+
+
+// ANSWER:5
+
+// esa function jo dosre  function ky argument mai call ho
+
+// ANSWER:6
+
+// let printName = (name) => {
+//     console.log(name);
+
+// }
+
+// let userName = () => {
+// }
+
+
+// printName(userName)
+
+
+// Answer : 7
+
+// filter() ka use araay pr filteration ky liye hota ha
+
+// Answer : 8
+
+// reduce ka purpose congrate ky liye hota ha yani do value ko milane ky liye
+
+// Answer : 9
+
+// find sirf single chz dhondne ky liye hota ha
+// find return main sigle element return krta ha
+//  or 
+// filter multiple chz ky liye 
+// filter return mai filteration array return karta ha
+
+// Answer : 10
+
+// map modification ky liye use hota ha 
+// map return main new modification array return karta ha
+// or
+// forEach iteration ky liye use hota ha
+// forEach return kuch bhi nh karta sirf undefine deta ha return main
+
+// Answer : 11
+
+// let nums=[1,2,3,4];
+// let doubledNums=nums.map((n)=>n*2)
+// console.log(doubledNums);
+
+// Answer : 12
+
+// let userName=["saqib","aqib","hasan","abdullah"]
+// let name=userName.map((name)=>name.toUpperCase())
+// console.log(name);
+
+// Answer : 13
+
+// let prices=[10,20,30,40,50]
+// pricesIncrease=prices.map((pri)=>pri+10)
+// console.log(pricesIncrease);
+
+
+// Answer : 14
+
+//  let userName=["saqib","aqib","hasan","abdullah"];
+//  updatedName=userName.map((name)=>`Mr.${name}`)
+//  console.log(updatedName);
+ 
+
+// Answer : 15
+
+// let nums=[5,6,7];
+// let squareNums=nums.map((nums)=>nums*nums);
+// console.log(squareNums);
+
+// Answer : 16
+
+// let num=[1,2,3,4,5,6];
+// let filterNums=num.filter((num)=>num%2==0)
+// console.log(filterNums);
+
+// Answer : 17
+
+//  let userName=["saqib","aqib","hasan","abdullah"];
+//  findName=userName.filter((name)=>name.length>5)
+//  console.log(findName);
+ 
+// Answer : 18
+
+//  let prices=[100,80,300,90,500]
+//  let findPrices=prices.filter((price)=>price>100)
+//  console.log(findPrices);
+ 
+// Answer : 19
